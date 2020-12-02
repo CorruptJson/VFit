5ff1bb15c131ea834602554af794184deb335b1e
JasonZFHuang <jasonhuangzf@gmail.com>
2020-12-02 11:28:09 -0800

    imported volley

---
+++ client/app/src/main/java/com/example/vfit/RequestHelper.java
@@ -0,0 +1,16 @@
+package com.example.vfit;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+
+import org.json.JSONObject;
+
+public class RequestHelper {
+    public final static String url = "https://vfit-btech.herokuapp.com/";
+
+    private RequestHelper() {
+        throw new AssertionError();
+    }
+
+}
